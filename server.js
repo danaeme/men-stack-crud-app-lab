00a374c37ac9c775d3f7658ffc4989332733aa14
David Anaeme <davidcanaeme@gmail.com>
2024-07-26 17:22:54 -0600

    Server is running

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ dotenv.config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
+//middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
 mongoose.connect(process.env.MONGODB_URI);
 //Connect to MONGODB
 mongoose.connection.on("connected", () => {
@@ -16,6 +20,12 @@ mongoose.connection.on("connected", () => {
 app.get("/", async (req, res) => {
     res.render("index.ejs");
   });
+
+  //server test
+  app.get("/test", (req, res) => {
+    console.log("GET /test route works")
+    res.send("Server is running")
+  })
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
